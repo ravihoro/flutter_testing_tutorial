510d2009c55eddd2741b3c2d6a1d49c8beb67256
Max <max.weber.consultant@outlook.com>
2021-01-11 11:06:49 +0100

    added todos to the integration test branch

--- test_driver/integration_test_driver.dart
+++ test_driver/integration_test_driver.dart
@@ -1,3 +1,1 @@
-import 'package:integration_test/integration_test_driver.dart';
-
-Future<void> main() => integrationDriver();
+// TODO 2: Create a main function and execute the integrationDriver function
